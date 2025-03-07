8d99e994795027414bf4b668c6fe4bf9b92d3776
Abdelrahman Fathi <abdelrahmanfathi73@gmail.com>
2025-03-07 08:11:27 +0200

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL = "https://crud-website-i6js.vercel.app/";
 
 // دالة لجلب البيانات
 export const getFetchData = async (setDataList) => {
